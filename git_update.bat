fbab697106e708b781503ceb4bfd89e0f667ccf5
Nima Askari (نیما عسکری) <nima.askari@gmail.com>
2026-07-21 18:58:27 +0200

    Update git_update.bat

--- git_update.bat
+++ git_update.bat
@@ -1,6 +1,6 @@
 @echo off
 :: 1. Navigate to your main repository folder (Change this path to your actual project folder)
-cd /d "C:\path\to\your\main\repo"
+cd /d "E:\GitHub\AhuraRTOS"
 
 echo =========================================
 echo Updating all submodules...
